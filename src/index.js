d31f6e0d68e9098620b0416fe266ad24166203bd
dejan <dejan.biljecki@codecentric.de>
2019-05-24 14:35:38 +0200

    added red heart tween on click event

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
-import logoImg from "./assets/yellow_heart.png";
+import yellowHeart from "./assets/yellow_heart.png";
+import redHeart from "./assets/redHeart.png";
 
 const config = {
   type: Phaser.AUTO,
@@ -10,16 +11,20 @@ const config = {
     preload: preload,
     create: create,
     update: update,
+    onObjectClicked: onObjectClicked
   }
 };
 
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("asteroid", logoImg);
+  this.load.image("yellowHeart", yellowHeart);
+  this.load.image("redHeart", redHeart);
 }
 
 function create() {
+  this.onObjectClicked = onObjectClicked;
+  this.easeOut = easeOut;
 
   // let hearts = [];
 
@@ -30,129 +35,159 @@ function create() {
   // this.add.image(200, 100, 'logo').setOrigin(0, 0);
 
   this.asteroids = [];
-this.positions = [];
-//   this.positions = [
-//     { x: 37, y: 176 },
-//     { x: 187, y: 66 },
-//     { x: 177, y: 406 },
-//     { x: 317, y: 256 },
-//     { x: 417, y: -10 },
-//     { x: 487, y: 336 },
-//     { x: 510, y: 116 },
-//     { x: 727, y: 186 },
-//     { x: 697, y: 10 },
-//     { x: 597, y: 216 },
-//     { x: 695, y: 366 },
-//     { x: 900, y: 76 },
-//     { x: 1008, y: 315 }
-// ];
+  this.positions = [];
 
-  for (let i = 0; i < 30; i++){
+  for (let i = 0; i < 100; i++) {
     this.positions.push({
-      x: 2000,
-      y: 2000,
-    })
+      x: 2500,
+      y: 2500
+    });
   }
 
+  for (let i = 0; i < this.positions.length; i++) {
+    let pos = this.positions[i];
 
-  for (let i = 0; i < this.positions.length; i++)
-  {
-      let pos = this.positions[i];
+    let therock = this.add.sprite(pos.x, pos.y, "yellowHeart");
 
-      let therock = this.add.sprite(pos.x, pos.y, 'asteroid');
+    therock.setData("vx", Math.random(0.005) - 0.0025);
+    therock.setData("vy", Math.random(0.005) - 0.0025);
+    therock.setData("id", i);
 
-      therock.setData('vx', Math.random(0.005)-0.0025);
-      therock.setData('vy', Math.random(0.005)-0.0025);
+    therock.setOrigin(0);
+    therock.setScale(Phaser.Math.FloatBetween(0.1, 0.3));
 
-      therock.setOrigin(0);
-      therock.setScale(Phaser.Math.FloatBetween(0.1, 0.3));
+    let rotationSpeed = 0;
 
-      this.asteroids.push(therock);
+    let willRotate = chance(15);
+    if (willRotate) rotationSpeed = (Math.random() - 0.5) / 10;
+
+    therock.setData("rotate", rotationSpeed);
+
+    therock.setInteractive();
+
+    this.asteroids.push(therock);
   }
 
+  this.input.on("gameobjectdown", this.onObjectClicked.bind(this));
 }
 
+function onObjectClicked(pointer, gameObject) {
+  console.log(gameObject);
+  if (gameObject) {
+    gameObject.setTexture("redHeart");
+    // this.asteroids = this.asteroids.filter(
+    //   heart => heart.getData("id") !== gameObject.getData("id")
+    // );
+    // gameObject.destroy();
 
-function update (time, delta)
-    {
-        for (let i = 0; i < this.asteroids.length; i++)
-        {
-            let therock = this.asteroids[i];
+    let self = this;
 
-            therock.x -= therock.getData('vx') * delta/4;
-            therock.y -= therock.getData('vy') * delta/4;
+    this.tweens.add({
+      targets: gameObject,
+      scaleX: 1,
+      scaleY: 1,
+      ease: "Sine.easeInOut",
+      duration: 300,
+      repeat: 0,
+      onComplete: onCompleteGrowHandler,
+      onCompleteParams: [self]
+    });
+  }
+}
 
-            //console.log(therock.getData('vx') * delta)
+function onCompleteGrowHandler(tween, targets, self) {
+  const tself = self;
+  self.tweens.add({
+    targets: targets[0],
+    scaleX: 0,
+    scaleY: 0,
+    ease: "Sine.easeOut",
+    duration: 300,
+    repeat: 0,
+    onComplete: onCompleteShrinkHandler,
+    onCompleteParams: [tself]
+  });
+}
 
+function onCompleteShrinkHandler(tween, targets, self) {
+  const gameObject = targets[0]
+  
+  self.asteroids = self.asteroids.filter(
+    heart => heart.getData("id") !== gameObject.getData("id")
+  );
+  console.log(self.asteroids.length)
+  gameObject.destroy();
+}
 
-            if (therock.x <= -400 || therock.x >= 1250 || therock.y < -400 || therock.y > 810)
-            {
-              
-                // console.log(therock.x, therock.y)
+function update(time, delta) {
+  for (let i = 0; i < this.asteroids.length; i++) {
+    let therock = this.asteroids[i];
 
-                let py = Math.round(Math.random());
-                let px = Math.round(Math.random());
+    therock.x -= (therock.getData("vx") * delta) / 4;
+    therock.y -= (therock.getData("vy") * delta) / 4;
 
-                if (px && py) {
-                  therock.x = Math.random(600)-300;
-                  therock.y = Math.random(400)-300;
-                  therock.setData('vy', -Math.random(0.1));
-                  therock.setData('vx', -Math.random(0.1));
-                }
+    therock.rotation += therock.getData("rotate");
 
-                if (px && !py) {
-                  therock.x = Math.random(600)-300;
-                  therock.y = Math.random(400)+800;
-                  therock.setData('vy', Math.random(0.1));
-                  therock.setData('vx', -Math.random(0.1));
-                }
+    if (
+      therock.x <= -400 ||
+      therock.x >= 1250 ||
+      therock.y < -400 ||
+      therock.y > 810
+    ) {
+      // console.log(therock.x, therock.y)
 
-                if (!px && py) {
-                  therock.x = Math.random(600)+1200;
-                  therock.y = Math.random(400)-300;
-                  therock.setData('vy', -Math.random(0.1));
-                  therock.setData('vx', Math.random(0.1));
-                }
+      let py = Math.round(Math.random());
+      let px = Math.round(Math.random());
 
-                if (!px && !py) {
-                  therock.x = Math.random(600)+1200;
-                  therock.y = Math.random(400)+800;
-                  therock.setData('vy', Math.random(0.1));
-                  therock.setData('vx', Math.random(0.1));
-                }
+      if (px && py) {
+        therock.x = Math.random(600) - 300;
+        therock.y = Math.random(400) - 300;
+        therock.setData("vy", -Math.random(0.1));
+        therock.setData("vx", -Math.random(0.1));
+      }
 
-                // therock.y = 0;
-                // therock.x = 0;
+      if (px && !py) {
+        therock.x = Math.random(600) - 300;
+        therock.y = Math.random(400) + 800;
+        therock.setData("vy", Math.random(0.1));
+        therock.setData("vx", -Math.random(0.1));
+      }
 
-                // console.log(py);
+      if (!px && py) {
+        therock.x = Math.random(600) + 1200;
+        therock.y = Math.random(400) - 300;
+        therock.setData("vy", -Math.random(0.1));
+        therock.setData("vx", Math.random(0.1));
+      }
 
-                //console.log(py)
-
-                // if (py) {
-                //   therock.y = Math.random(800);
-                //   therock.setData('vy', Math.random(0.1));
-                // } 
-                // else {
-                //   therock.y = 300;
-                //   therock.setData('vy', Math.random(0.1));
-                //   console.log(therock.y)
-                // }
-
-                // if (px) {
-                //   therock.x = Math.random(1224);
-                //   therock.setData('vx', -Math.random(0.1));
-                // } 
-                // else {
-                //   therock.x = ;
-                //   therock.setData('vx', +Math.random(0.1));
-                // }
-
-
-
-                //therock.x = 400;
-                // therock.y = 1224;
-                // therock.setData('vx', Math.random(0.1)-0.05);
-                // therock.setData('vy', Math.random(0.1)-0.05);
-            }
-        }
+      if (!px && !py) {
+        therock.x = Math.random(600) + 1200;
+        therock.y = Math.random(400) + 800;
+        therock.setData("vy", Math.random(0.1));
+        therock.setData("vx", Math.random(0.1));
+      }
     }
+  }
+}
+
+function chance(x) {
+  let c = Math.random() * 100;
+  //console.log(c)
+  return x > c;
+}
+
+const easeOut = (self, gameObject) => {
+  console.log("easeout params", self, gameObject);
+
+  self.tweens.add({
+    targets: gameObject,
+    scaleX: 1,
+    scaleY: 1,
+    ease: "Sine.easeInOut",
+    duration: 300,
+    // delay: i * 50,
+    repeat: 0,
+    yoyo: true
+    //onComplete: (self, gameObject) => { self.easeOut(self, gameObject)},
+  });
+};
